64abacefb09b744b725981811992668756100da2
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-08-25 16:11:48 +0430

    Initial commit

---
+++ app/src/main/java/com/snap/snapNewsApp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.snap.snapNewsApp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.snap.snapNewsApp.ui.theme.SnapNewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SnapNewsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+
+//4306da9b007544c99aefb13e7ef73d5c
