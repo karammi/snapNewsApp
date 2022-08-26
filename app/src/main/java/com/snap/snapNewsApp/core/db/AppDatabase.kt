1511256da61ee50790ef6fa81bb98fae25736da7
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-08-26 09:41:04 +0430

    chore: Added ktx library to support suspend functions in room

--- app/src/main/java/com/snap/snapNewsApp/core/db/AppDatabase.kt
+++ app/src/main/java/com/snap/snapNewsApp/core/db/AppDatabase.kt
@@ -2,7 +2,10 @@ package com.snap.snapNewsApp.core.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.snap.snapNewsApp.news.data.dataSource.local.dao.PressDao
 import com.snap.snapNewsApp.news.data.dataSource.local.model.PressEntity
 
 @Database(entities = [PressEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase()
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun providePressDao(): PressDao
+}
