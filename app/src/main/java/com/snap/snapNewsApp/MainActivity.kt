153430b00c236b323ca3d489f08cc0b306e619ba
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-08-26 09:22:46 +0430

    chore: Added andoridEntryPoint to mainActivity class to be able to inject deps

--- app/src/main/java/com/snap/snapNewsApp/MainActivity.kt
+++ app/src/main/java/com/snap/snapNewsApp/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import com.snap.snapNewsApp.core.ui.theme.SnapNewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/snap/snapNewsApp/core/di/module/NetworkModule.kt
+++ app/src/main/java/com/snap/snapNewsApp/core/di/module/NetworkModule.kt
@@ -4,6 +4,8 @@ import com.snap.snapNewsApp.BuildConfig
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -12,6 +14,7 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
 
     @Provides
